30432a4c255589bdaef35be016750443fac5678a
Balazs Juhasz <juhaszbal@gmail.com>
2021-01-29 19:07:03 +0100

    finish draggin of cards

--- script.js
+++ script.js
@@ -57,10 +57,6 @@ const updateSavedColumns = () => {
 
 // Create DOM Elements for each list item
 const createItemEl = (columnEl, column, item, index) => {
-  // console.log('columnEl:', columnEl);
-  // console.log('column:', column);
-  // console.log('item:', item);
-  // console.log('index:', index);
   // List Item
   const listEl = document.createElement('li');
   listEl.classList.add('drag-item');
@@ -104,12 +100,33 @@ const updateDOM = () => {
   });
 
   // Run getSavedColumns only once, Update Local Storage
+  updateSavedColumns();
+};
+
+// Allows arrays to reflect Drag and Drop items
+const rebuildArrays = () => {
+  backlogListArray = [];
+  for (let i = 0; i < backlogList.children.length; i++) {
+    backlogListArray.push(backlogList.children[i].textContent);
+  }
+  progressListArray = [];
+  for (let i = 0; i < progressList.children.length; i++) {
+    progressListArray.push(progressList.children[i].textContent);
+  }
+  completeListArray = [];
+  for (let i = 0; i < completeList.children.length; i++) {
+    completeListArray.push(completeList.children[i].textContent);
+  }
+  onHoldListArray = [];
+  for (let i = 0; i < onHoldList.children.length; i++) {
+    onHoldListArray.push(onHoldList.children[i].textContent);
+  }
+  updateDOM();
 };
 
 // When Item Starts Dragging
 const drag = (e) => {
   draggedItem = e.target;
-  console.log('draggedItem', draggedItem);
 };
 
 // Column Allows for Item to Drop
@@ -133,6 +150,7 @@ const drop = (e) => {
   // Add Item to Column
   const parent = listColumns[currentColumn];
   parent.appendChild(draggedItem);
+  rebuildArrays();
 };
 
 // On Load
